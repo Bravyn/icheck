46a60d9556555cee4d4dc0f2011b29bb2cad79ff
root <root@localhost.localdomain>
2021-06-28 07:12:00 -0400

    now using actix-web

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,34 @@
-use std::io::prelude::*;
-use std::net::{ TcpListener, TcpStream };
+//These actix-web types and traits will let us 
+//create a web server rapidly
 
-fn main() {
-    println!("Hello, world!{}");
-    start();
+use actix_web::{get, post, web, App, HttpResponse,
+    HttpServer, Responder };
+//first off, let us create our routes
+
+#[get("/")]//macro for route handling functions
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Welcome to icheck, the worlds' most advanced recon server!")
 }
 
-
-pub fn handle_client(mut stream: TcpStream) {
-    let mut buf = [0; 4096];
-
-    stream.read(&mut buf).unwrap();
-
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
-
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
-
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder{
+    HttpResponse::Ok().body(req_body)
 }
 
-pub fn start() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:80")?;
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(index)
+            .service(echo)
+            .route("/hey", web::get().to(manual_hello))
+    })
+    .bind("127.0.0.1:3000")?
+    .run()
+    .await  
     
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
-        handle_client(stream);
-    }
-    Ok(())
 }
